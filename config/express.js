594efcf56b8c53abe69a9b758c712e5b642bfbc8
andersonnagelo <31452157+andersonnagelo@users.noreply.github.com>
2018-05-15 17:10:31 -0300

    Primeiro commit

---
+++ config/express.js
@@ -0,0 +1,9 @@
+let express = require ('express');
+
+module.exports = function(){
+    let app = express ();
+
+    app.set ("port", 3000);
+    app.use(express.static('./public'));
+    return app;
+}
